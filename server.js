7d6aea17819439639dedb067465ac39ff591f827
Ngc_khanh2915 <khanhnguyen27th@gmail.com>
2026-03-13 23:38:06 +0700

    fix port for render deploy

--- server.js
+++ server.js
@@ -81,6 +81,8 @@ app.get("/thietbi", async (req, res) => {
   res.json(data);
 });
 
-app.listen(3000, () => {
-  console.log("Server running at http://localhost:3000");
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running");
 });
